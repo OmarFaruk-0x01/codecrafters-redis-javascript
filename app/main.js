0a895749d9798f361a7a55a5253ffca49d6c5acc
Omar Faruk <43259457+OmarFaruk-0x01@users.noreply.github.com>
2022-12-01 18:43:25 +0600

    Pass Stage 7

--- app/main.js
+++ app/main.js
@@ -1,13 +1,45 @@
 const net = require("net");
 
 const DB = {};
+const ExpireDB = {};
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Custom Redis Server Stating...");
 
-// Uncomment this block to pass the first stage
+function set({ key, data, callback, expiry = false }) {
+  DB[key] = data;
+  if (expiry) {
+    ExpireDB[key] = {
+      createdAt: Date.now(),
+      expiry: 0 + +expiry,
+    };
+  }
+  callback();
+}
+
+function get(key, callback) {
+  if (Object.keys(DB).includes(key)) {
+    if (Object.keys(ExpireDB).includes(key)) {
+      const time = Date.now() - ExpireDB[key].createdAt;
+      if (time > ExpireDB[key].expiry) {
+        delete DB[key];
+        delete ExpireDB[key];
+        return callback({ data: null, error: "timeover" });
+      }
+    }
+    return callback({ data: DB[key], error: null });
+  }
+  return callback({ data: null, error: "empty" });
+}
+
+function extractArgs(array, index) {
+  return array
+    ?.at(index)
+    ?.replace(/['"]/g, "")
+    ?.slice(1, array?.at(index).length);
+}
+
 const server = net.createServer((connection) => {
-  // Handle connection
   connection.on("error", (err) => {
     console.log("Error: ", err);
   });
@@ -15,7 +47,8 @@ const server = net.createServer((connection) => {
     const dataArray = data.toString().replace(/\r\n/g, "").split("$");
     const commandArrayLength = 0 + +dataArray[0].trim().replace("*", "");
     const command = dataArray[1].trim().slice(1, dataArray[1].length);
-    console.log(dataArray, commandArrayLength, command);
+    const key = extractArgs(dataArray, 2);
+
     switch (command.toLowerCase()) {
       case "ping":
         connection.write(Buffer.from("+PONG\r\n", "utf-8"), (err) => {});
@@ -29,62 +62,78 @@ const server = net.createServer((connection) => {
             )
           );
         } else {
-          connection.write(
-            Buffer.from(
-              `+${dataArray
-                ?.at(2)
-                ?.replace(/['"]/g, "")
-                .slice(1, dataArray?.at(2).length)}\r\n`,
-              "utf-8"
-            )
-          );
+          const value = extractArgs(dataArray, 2);
+          connection.write(Buffer.from(`+${value}\r\n`, "utf-8"));
           break;
         }
       case "get":
         if (commandArrayLength !== 2) {
           connection.write(
             Buffer.from(
-              `+(error) ERR wrong number of arguments for 'echo' command\r\n`,
+              `+(error) ERR wrong number of arguments for 'get' command\r\n`,
               "utf-8"
             )
           );
         } else {
-          const key = dataArray
-            ?.at(2)
-            ?.replace(/['"]/g, "")
-            .slice(1, dataArray?.at(2).length);
-          if (!Object.keys(DB).includes(key)) {
-            connection.write(Buffer.from(`+(nil)\r\n`, "utf-8"));
-          } else {
-            connection.write(Buffer.from(`+${DB[key]}\r\n`, "utf-8"));
-          }
+          get(key, ({ data, error }) => {
+            if (Boolean(error)) {
+              connection.write(Buffer.from(`$-1\r\n`, "utf-8"));
+              return;
+            }
+            connection.write(Buffer.from(`+${data}\r\n`, "utf-8"));
+          });
           break;
         }
       case "set":
-        if (commandArrayLength !== 3) {
+        const value = extractArgs(dataArray, 3);
+
+        if (commandArrayLength === 3) {
+          try {
+            set({
+              key,
+              data: value,
+              callback: () => {
+                connection.write(Buffer.from(`+OK\r\n`, "utf-8"));
+                console.log(DB);
+              },
+            });
+          } catch (err) {
+            connection.write(Buffer.from(`$-1\r\n`, "utf-8"));
+          }
+        } else if (commandArrayLength === 5) {
+          const subCommand = extractArgs(dataArray, 4);
+          console.log(subCommand);
+          switch (subCommand.toLowerCase()) {
+            case "px": {
+              const expiryMilliseconds = extractArgs(dataArray, 5);
+              set({
+                key,
+                data: value,
+                expiry: expiryMilliseconds,
+                callback: () => {
+                  console.log("DB: ", DB);
+                  console.log("Expire DB: ", ExpireDB);
+                  connection.write(Buffer.from(`+OK\r\n`, "utf-8"));
+                },
+              });
+            }
+          }
+        } else {
           connection.write(
             Buffer.from(
               `+(error) ERR wrong number of arguments for 'set' command\r\n`,
               "utf-8"
             )
           );
-        } else {
-          try {
-            DB[
-              dataArray
-                ?.at(2)
-                ?.replace(/['"]/g, "")
-                .slice(1, dataArray?.at(2).length)
-            ] = dataArray
-              ?.at(3)
-              ?.replace(/['"]/g, "")
-              .slice(1, dataArray?.at(3).length);
-
-            connection.write(Buffer.from(`+OK\r\n`, "utf-8"));
-            console.log(DB);
-          } catch (err) {}
-          break;
         }
+        break;
+      default:
+        connection.write(
+          Buffer.from(
+            `+(error) Unknown command try:\nSET <key> <value> [PX <expiry>],\nGET <key>,\nECHO <message>,\nPING\r\n`,
+            "utf-8"
+          )
+        );
     }
   });
 });
