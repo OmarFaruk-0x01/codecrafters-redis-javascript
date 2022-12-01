e5d4420ad9865961235f4aaf741a20feb9887adb
Omar Faruk <43259457+OmarFaruk-0x01@users.noreply.github.com>
2022-12-01 15:47:35 +0600

    Pass Stage 6

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 const net = require("net");
 
+const DB = {};
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
@@ -14,12 +16,12 @@ const server = net.createServer((connection) => {
     const commandArrayLength = 0 + +dataArray[0].trim().replace("*", "");
     const command = dataArray[1].trim().slice(1, dataArray[1].length);
     console.log(dataArray, commandArrayLength, command);
-    switch (command) {
+    switch (command.toLowerCase()) {
       case "ping":
         connection.write(Buffer.from("+PONG\r\n", "utf-8"), (err) => {});
         break;
       case "echo":
-        if (commandArrayLength > 2) {
+        if (commandArrayLength !== 2) {
           connection.write(
             Buffer.from(
               `+(error) ERR wrong number of arguments for 'echo' command\r\n`,
@@ -38,8 +40,54 @@ const server = net.createServer((connection) => {
           );
           break;
         }
+      case "get":
+        if (commandArrayLength !== 2) {
+          connection.write(
+            Buffer.from(
+              `+(error) ERR wrong number of arguments for 'echo' command\r\n`,
+              "utf-8"
+            )
+          );
+        } else {
+          const key = dataArray
+            ?.at(2)
+            ?.replace(/['"]/g, "")
+            .slice(1, dataArray?.at(2).length);
+          if (!Object.keys(DB).includes(key)) {
+            connection.write(Buffer.from(`+(nil)\r\n`, "utf-8"));
+          } else {
+            connection.write(Buffer.from(`+${DB[key]}\r\n`, "utf-8"));
+          }
+          break;
+        }
+      case "set":
+        if (commandArrayLength !== 3) {
+          connection.write(
+            Buffer.from(
+              `+(error) ERR wrong number of arguments for 'set' command\r\n`,
+              "utf-8"
+            )
+          );
+        } else {
+          try {
+            DB[
+              dataArray
+                ?.at(2)
+                ?.replace(/['"]/g, "")
+                .slice(1, dataArray?.at(2).length)
+            ] = dataArray
+              ?.at(3)
+              ?.replace(/['"]/g, "")
+              .slice(1, dataArray?.at(3).length);
+
+            connection.write(Buffer.from(`+OK\r\n`, "utf-8"));
+            console.log(DB);
+          } catch (err) {}
+          break;
+        }
     }
   });
 });
+
 //
 server.listen(6379, "127.0.0.1");
